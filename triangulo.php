012e54117b92a69055cbc18012be45ab0e3d0bfd
David <7338.joan23@fje.edu>
2024-01-12 17:59:07 +0100

    arreglos

--- triangulo.php
+++ triangulo.php
@@ -1,15 +1,22 @@
 <?php
 // Clase para el triángulo
 class Triangulo extends FiguraGeometrica implements PerimetroM {
+    // Propiedades de la clase
     protected $lado2;
     protected $altura;
 
+    // Constructor de la clase
     public function __construct($base, $lado2, $altura = null) {
+        // Llama al constructor de la clase base (FiguraGeometrica) con el nombre "Triángulo" y la longitud de la base
         parent::__construct("Triángulo", $base);
+
+        // Inicializa las propiedades del triángulo
         $this->lado2 = $lado2;
-        $this->altura = $altura ?? $lado2 / 2; // Utiliza la mitad del lado2 si altura no se proporciona
+        // Utiliza la altura proporcionada, o la mitad del lado2 si la altura no se proporciona
+        $this->altura = $altura ?? $lado2 / 2;
     }
 
+    // Métodos de acceso para obtener y establecer el valor del lado2
     public function getLado2() {
         return $this->lado2;
     }
@@ -18,6 +25,7 @@ class Triangulo extends FiguraGeometrica implements PerimetroM {
         $this->lado2 = $lado2;
     }
 
+    // Métodos de acceso para obtener y establecer el valor de la altura
     public function getAltura() {
         return $this->altura;
     }
@@ -26,13 +34,13 @@ class Triangulo extends FiguraGeometrica implements PerimetroM {
         $this->altura = $altura;
     }
 
+    // Implementación del método para calcular el área del triángulo
     public function area() {
-        // Implementa el cálculo del área para el triángulo con base y altura
         return ($this->getLado1() * $this->altura) / 2;
     }
 
+    // Implementación del método para calcular el perímetro del triángulo
     public function perimetro() {
-        // Implementa el cálculo del perímetro para el triángulo
         return $this->getLado1() + $this->lado2 + parent::getLado1();
     }
 }
